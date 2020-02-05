adc5f3c4d176d6ac97b121f3b5a9cb925f1ff684
sreeram-pt3552 <sreeramktm027@gmail.com>
2020-02-05 12:10:10 +0530

    before deleting demo

--- src/temporary/SaveImage.java
+++ src/temporary/SaveImage.java
@@ -13,8 +13,11 @@ public class SaveImage {
         OutputStream out=null;
             try{
         URL myurl=new URL("https://www.google.com/images/srpr/logo3w.png");
+        String fileName = myurl.getFile();
+        String destname= "C:\\Users\\sreeram-pt3552\\Desktop"+fileName.substring(fileName.lastIndexOf("/"));
+      //  System.out.println(fileName+" "+destname);
         in=myurl.openStream();
-        out=new FileOutputStream("one.jpg");
+        out=new FileOutputStream(destname);
         byte b[]=new byte[2048];
         int len;
         while ((len=in.read(b))!=-1){
